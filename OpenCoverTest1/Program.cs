c4f678993eb792e40d3470d03e44175cc3dcdb35
Masaru Tsuchiyama <m.tmatma@gmail.com>
2017-02-19 11:50:51 +0900

    OpenCover のテストプログラムを追加

---
+++ OpenCoverTest1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCoverTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+
+            foreach (var str in args)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
