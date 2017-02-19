5f58e8c6cb9b60b28b0ff2e44ed9f9022af2cd28
Masaru Tsuchiyama <m.tmatma@gmail.com>
2017-02-19 14:54:38 +0900

    DLL のプロジェクトを追加

---
+++ OpenCoverTest2/ClassLibrary/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCoverTest2
+{
+    public class Class1
+    {
+        public static string GetString(int index)
+        {
+            if (index == 0)
+            {
+                return "String1";
+            }
+            else
+            {
+                return "String2";
+            }
+        }
+    }
+}
+
---
+++ OpenCoverTest2/OpenCoverTest2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCoverTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.GetString(0));
+            Console.WriteLine(Class1.GetString(1));
+        }
+    }
+}
